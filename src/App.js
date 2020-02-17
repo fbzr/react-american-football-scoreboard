2d7b75f0355ab99e75d08b4425a33f3685d8f8b4
fbzr <fabricio.spb@gmail.com>
2020-02-17 10:09:51 -0800

    organize components tree

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import Scoreboard from "./components/Scoreboard";
+import Buttons from "./components/Buttons";
 import "./App.css";
 
 function App() {
+  const [score, setScore] = useState({
+    home: 0,
+    away: 0
+  });
+
+  const homeTouchdown = () => {
+      setScore({
+          ...score,
+          home: score.home + 7
+      });
+  }
+
+  const homeFieldGoal = () => {
+      setScore({
+          ...score,
+          home: score.home + 3
+      });
+  }
+
+  const awayTouchdown = () => {
+      setScore({
+          ...score,
+          away: score.away + 7
+      });
+  }
+
+  const awayFiledGoal = () => {
+      setScore({
+          ...score,
+          away: score.away + 3
+      });
+  }
+
+  const updateScore = {
+      homeTouchdown,
+      homeFieldGoal,
+      awayTouchdown,
+      awayFiledGoal
+  }
+
   return (
-    <Scoreboard />
+    <div className="container">
+      <Scoreboard score={score} />
+      <Buttons updateScore={updateScore} />
+    </div>
   );
 }
 
--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -3,56 +3,13 @@ import BottomRow from "./BottomRow";
 import TopRow from "./TopRow";
 import Buttons from "./Buttons";
 
-const Scoreboard = () => {
-    const [score, setScore] = useState({
-        home: 0,
-        away: 0
-    });
-    
-    const homeTouchdown = () => {
-        setScore({
-            ...score,
-            home: score.home + 7
-        });
-    }
-
-    const homeFieldGoal = () => {
-        setScore({
-            ...score,
-            home: score.home + 3
-        });
-    }
-
-    const awayTouchdown = () => {
-        setScore({
-            ...score,
-            away: score.away + 7
-        });
-    }
-    
-    const awayFiledGoal = () => {
-        setScore({
-            ...score,
-            away: score.away + 3
-        });
-    }
-
-    const updateScore = {
-        homeTouchdown,
-        homeFieldGoal,
-        awayTouchdown,
-        awayFiledGoal
-    }
-
-
+const Scoreboard = ({score}) => {
+    const {home, away} = score;
     return (
-        <div className="container">
-            <section className="scoreboard">
-                <TopRow homeScore={score.home} awayScore={score.away} />
-                <BottomRow />
-            </section>
-            <Buttons updateScore={updateScore} />
-        </div>
+        <section className="scoreboard">
+          <TopRow homeScore={home} awayScore={away} />
+          <BottomRow />
+        </section>
     )
 }
 
